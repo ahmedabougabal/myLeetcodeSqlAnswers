4e84440f47144ceeb1d20ae07e80e6f3aa102d95
ahmedabougabal <ahmedabougabal@live.com>
2025-01-16 21:24:23 +0200

    feat: adds another correct solution where i have used a left join to return the customers that have no ids in the orders table using the filtering part 'where' exp: 1. LEFT JOIN preserves all records from Customers table (left table) 2. Matches customer IDs with Orders table (right table) 3. When no match found in Orders, customerId becomes NULL 4. WHERE clause filters for these NULL values 5. Returns only customer names where no matching order exists

--- customersWhoNeverOrder.sql
+++ customersWhoNeverOrder.sql
@@ -3,3 +3,10 @@
 select c.name as Customers
 from Customers c
 where id not in (select customerId from Orders); 
+
+
+-- 2nd correct solution
+select c.name as Customers
+from Customers c 
+left join Orders o on c.id = o.customerId
+where o.customerId IS NULL;
