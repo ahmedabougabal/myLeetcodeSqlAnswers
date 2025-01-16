1e3da4bbf18ef15d0e3c3277206994e65e7705dc
ahmedabougabal <ahmedabougabal@live.com>
2025-01-16 22:12:10 +0200

    feat: answers question 2 and add an index on the column 'order_date' from the table 'orders' as requested

--- interview_question.sql
+++ interview_question.sql
@@ -9,5 +9,15 @@ order by totalSpent Desc
 LIMIT 5
 
 
+-- 2. Explain what indexes are and how they improve query performance.
 
+-- indexes are b+ or b- data structures and are used in this context to provide fast data (rows) retrieval
+-- for frequent accessed data, (not optimal to apply an index to each column as this may cause a peformance overhead
+-- or a an excess storage usage as indexes consume storage space due to both pointers to actual data rows and 
+-- a copy of the indexed column. 
+
+-- types : single col index, composite index , unique index , clustered index , non clustered index  
+
+---  Write a query to add an index to the order_date column in the orders table.
+create INDEX date_index on (orders)order_date ;
 
