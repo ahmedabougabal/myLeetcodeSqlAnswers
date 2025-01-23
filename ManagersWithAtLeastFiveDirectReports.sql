ea27b0cf399727963341846896db699c4159fd65
ahmedabougabal <ahmedabougabal@live.com>
2025-01-23 21:59:13 +0200

    feat(sql): writes an optimized query to retrieve managers with at least five (or more) direct reports using join and group by uses inner join to match employees with their managers grouped results by e.managerId to count direct reports filters managers with the condition : 'having count(e.managerId) >= 5 ;' selects manager's name from the joined table :) utilizes join instead of sub query for a better runtime/performance

---
+++ ManagersWithAtLeastFiveDirectReports.sql
@@ -0,0 +1,7 @@
+-- Write your MySQL query statement below
+select m.name as name     
+from Employee e 
+inner join Employee m on
+e.managerId = m.id 
+group by e.managerId 
+having count(e.managerId) >= 5 ;
