cbbebbb22747026fd2e2458c03f0088b9eda3b97
ahmedabougabal <ahmedabougabal@live.com>
2025-01-27 20:40:36 +0200

    feat: Add alternative solution for LeetCode 1934. Confirmation Rate using IFNULL and NULLIF
    - Implemented a second solution using IFNULL and NULLIF to handle division by zero and null cases.
    - IFNULL ensures a default value of 0.00 for users with no confirmations.
    - NULLIF prevents division by zero by returning null when the denominator is zero.
    - used sql aggregation.
    - Both solutions achieve the same result, but this approach uses SQL functions for cleaner edge case handling.

--- confirmation_rate.sql
+++ confirmation_rate.sql
@@ -7,3 +7,15 @@ from Signups s
 left join Confirmations c
 on s.user_id = c.user_id
 group by user_id;
+
+-- # Write your MySQL query statement below
+select s.user_id , 
+IFNULL(round(count(case when c.action = 'confirmed' then 1 end)/
+NULLIF(count(c.action)
+,0)
+, 2),0.00)
+as confirmation_rate 
+from Signups s 
+left join Confirmations c
+on s.user_id = c.user_id
+group by user_id;
