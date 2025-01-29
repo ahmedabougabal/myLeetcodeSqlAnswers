560a3a405a4e5ea32caf4ad0fea2e87859ee6e81
ahmedabougabal <ahmedabougabal@live.com>
2025-01-30 00:05:50 +0200

    feat: Add Robust Calculation of Average Price for All Products
    This commit includes handling edge cases:
    
        Products with no sales.
        Division by zero errors.
        NULL values caused by missing sales data.
        Rounding of results to two decimal places to match the given output

--- AverageSellingPrice.sql
+++ AverageSellingPrice.sql
@@ -1,6 +1,7 @@
-select p.product_id, sum(p.price*u.units)/sum(u.units)
+select p.product_id, case when SUM(u.units) = 0 or SUM(u.units) = 0 is NULL then 0 else round(sum(p.price*u.units)/sum(u.units),2) end
 as average_price
 from Prices p
-inner join UnitsSold u 
+left join UnitsSold u 
 on p.product_id = u.product_id 
+and  u.purchase_date between p.start_date and p.end_date 
 group by p.product_id;
