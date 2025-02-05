80128bdef09dcae4fe4aa090dcf47a3fdda08598
ahmedabougabal <ahmedabougabal@live.com>
2025-02-05 14:05:25 +0200

    Implement immediate delivery percentage calculation using CTE and ROW_NUMBER window function
    - Added a CTE to compute each customer's first order based on order_date.
    - Utilized ROW_NUMBER() to assign a sequential order to orders per customer using the where condition (where rn = 1) to return the first order of each customer so where here is like a filter to only compute the 'immediate' only orders and calc the % correclty
    - Calculated the immediate order percentage by checking using case when else end

--- ImmediateFoodDeliveryII.sql
+++ ImmediateFoodDeliveryII.sql
@@ -20,3 +20,35 @@ where
     group by
       customer_id
   );
+
+-- ! approach 2
+with
+  first_orders as (
+    select
+      delivery_id,
+      customer_id,
+      order_date,
+      customer_pref_delivery_date,
+      row_number() over (
+        partition by
+          customer_id
+        order by
+          order_date
+      ) as rn
+    from
+      delivery
+  )
+select
+  round(
+    100 * sum(
+      case
+        when order_date = customer_pref_delivery_date then 1
+        else 0
+      end
+    ) / count(*),
+    2
+  ) as immediate_percentage
+from
+  first_orders
+where
+  rn = 1;
