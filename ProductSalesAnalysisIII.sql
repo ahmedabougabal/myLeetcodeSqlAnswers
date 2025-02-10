980ae60424c24d6a10ac6461be5ac193422902b0
ahmedabougabal <ahmedabougabal@live.com>
2025-02-10 15:31:07 +0200

    feat(sql): adds the Common table expression approach based query that computes each product's earliest sale year using MIN(year) and GROUP BY product_id. - Joined the CTE with the Sales table on product_id and the computed first_year to retrieve the corresponding quantity and price.

--- ProductSalesAnalysisIII.sql
+++ ProductSalesAnalysisIII.sql
@@ -15,3 +15,26 @@ where
     group by
       product_id
   );
+
+--! CTE approach 
+with
+  new_table as (
+    select
+      product_id,
+      quantity,
+      price,
+      min(year) as first_year
+    from
+      Sales
+    group by
+      product_id
+  )
+select
+  s.product_id,
+  s.year as first_year,
+  s.quantity,
+  s.price
+from
+  Sales s
+  join new_table nt on s.product_id = nt.product_id
+  and s.year = nt.first_year;
